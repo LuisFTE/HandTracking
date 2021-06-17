f9fc315f3fe912fcc48acd78ce574ff5d3032797
Luis Tamborrell <luis.tamborrell23@gmail.com>
2021-06-17 00:38:24 -0500

    Updating Git ignore and pushing in starter code

---
+++ Code/HandTrackingProject/HandTrackingMinCode.py
@@ -0,0 +1,63 @@
+import cv2
+import mediapipe as mp
+import time
+
+# Using Camera "1" for video capture most will need to use camera "0"
+cap = cv2.VideoCapture(1)
+
+# Initialize mp to detect hands
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
+# Initializing previous time and current time to calculate fps later on
+pTime = 0
+cTime = 0
+
+# Read camera feed
+while True:
+    success, img = cap.read()
+
+    # Convert BGR image into RGB
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    # Process image using mp.Hands
+    results = hands.process(imgRGB)
+    # print(results.multi_hand_landmarks)
+
+    # if the detection is detecting landmarks
+    if results.multi_hand_landmarks:
+        # We will loop to see how many hands are being detected
+        for handLandmarks in results.multi_hand_landmarks:
+
+            # We are looping to identify the id of the landmark and the position in pixels
+            for id, landmark in enumerate(handLandmarks.landmark):
+
+                # Height, Width, Color Channel = image shape (height and width in pixels
+                h, w, c = img.shape
+
+                # Calculate the pixel location of landmarks (landmark.x and .y are the x y ratio of the image we are
+                # multiplying by the actual size to find the pixel location)
+                cx, cy = int(landmark.x * w), int(landmark.y * h)
+                # print(id, cx, cy)
+
+                # We are drawing yellow circles on the landmarks 4, 8, 12, 16, and 20 (the tips of the fingers)
+                if id == 4 or id == 8 or id == 12 or id == 16 or id == 20:
+                    cv2.circle(img, (cx, cy), 10, (0, 255, 255), cv2.FILLED)
+
+            # Draw the landmarks on img (We are drawing on the img and not on the rgb because we are outputing the img
+            # not the RGBimg)
+            # handLandmarks is the points and the mp.Hands.HAND_CONNECTIONS are the connecting lines
+            mpDraw.draw_landmarks(img, handLandmarks, mpHands.HAND_CONNECTIONS)
+
+    # Calculating FPS
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+
+    # Outputting fps at 600x30 with font hershey, size 2, green, 2 thickness
+    cv2.putText(img, str(int(fps)), (600, 30), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2)
+
+    # Display image
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
